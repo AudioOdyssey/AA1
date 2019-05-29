6500c6895f8a24f3d14db06623928e8f44fb7d7d
sonnyghosh <ghoshsonny10@gmail.com>
2019-05-29 11:10:29 -0400

    added backend code to allow users to sign up

---
+++ app/ios/AudioOdyssey/AudioOdysseyTests/AudioOdysseyTests.swift
@@ -0,0 +1,34 @@
+//
+//  AudioOdysseyTests.swift
+//  AudioOdysseyTests
+//
+//  Created by Kyle Maiorana on 5/24/19.
+//  Copyright © 2019 SKAZ. All rights reserved.
+//
+
+import XCTest
+@testable import AudioOdyssey
+
+class AudioOdysseyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
