6024aba33c6bd69a36027a9e2c89b293a7af038c
sonnyghosh <ghoshsonny10@gmail.com>
2019-06-03 15:47:15 -0400

    Weirdness

---
+++ web/models/storyevent.py
@@ -0,0 +1,9 @@
+class StoryEvent:
+    def __init__(self, story_id, event_id, event_name, event_description, event_location_id, event_is_global):
+        self.story_id = story_id
+        self.event_id = event_id
+        self.event_name = event_name
+        self.event_description = event_description
+        self.event_location_id = event_location_id
+        self.event_is_global = event_is_global
+  
