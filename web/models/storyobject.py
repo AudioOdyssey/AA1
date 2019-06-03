ac7d439cf6ba46097915cccf512b7fbba75bbde2
sonnyghosh <ghoshsonny10@gmail.com>
2019-06-03 15:45:36 -0400

    added more stuff to the object clss

--- web/models/storyobject.py
+++ web/models/storyobject.py
@@ -1,10 +1,41 @@
+import pymysql
+import pymysql.cursors
+import sys
+
 class StoryObject:
+    story_id = 0
+    obj_id = 0
+    obj_name = ""
+    obj_description = ""
+    can_pickup_obj = 0
+    obj_starting_loc = 0
+    is_hidden = 0
+    unhide_event_id = 0
+
+    REGION = 'us-east-2b'
+
+    rds_host = "audio-adventures-dev.cjzkxyqaaqif.us-east-2.rds.amazonaws.com"
+    name = "AA_admin"
+    rds_password = "z9QC3pvQ"
+    db_name = "audio_adventures_dev"
+    
+
     def __init__(self, story_id, obj_id, obj_name, obj_description, can_pickup_obj, obj_starting_loc, is_hidden, unhide_event_id):
         self.story_id = story_id
-        self.obj_id = obj_id
+
         self.obj_name = obj_name
+
         self.obj_description = obj_description
+
         self.can_pickup_obj = can_pickup_obj
+
         self.obj_starting_loc = obj_starting_loc
+
         self.is_hidden = is_hidden
+
         self.unhide_event_id = unhide_event_id
+
+    def add_to_server(self):
+        conn = pymysql.connect(self.rds_host, user = self.name, passwd = self.rds_password, db = self.db_name, connect_timeout = 5, cursorclass = pymysql.cursors.DictCursor)
+        with conn.cursor() as cur:
+            pass
