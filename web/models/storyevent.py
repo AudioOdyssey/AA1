03548fb534f572147714cedad5ccae508689a036
Zoe Drasner <50959577+zdrasner@users.noreply.github.com>
2019-06-03 17:01:08 -0400

    weirdness

--- web/models/storyevent.py
+++ web/models/storyevent.py
@@ -1,4 +1,6 @@
 class StoryEvent:
+    event_location_name = ""
+
     def __init__(self, story_id, event_id, event_name, event_description, event_location_id, event_is_global):
         self.story_id = story_id
         self.event_id = event_id
