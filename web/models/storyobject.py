5c42468c668270c5c8ef9797844a090342fcb764
sonnyghosh <ghoshsonny10@gmail.com>
2019-06-04 10:14:04 -0400

    Update storyobject.py

--- web/models/storyobject.py
+++ web/models/storyobject.py
@@ -2,6 +2,7 @@ import pymysql
 import pymysql.cursors
 import sys
 
+import json
 class StoryObject:
     story_id = 0
     obj_id = 0
@@ -20,7 +21,7 @@ class StoryObject:
     db_name = "audio_adventures_dev"
     
 
-    def __init__(self, story_id, obj_id, obj_name, obj_description, can_pickup_obj, obj_starting_loc, is_hidden, unhide_event_id):
+    def __init__(self, story_id, obj_name, obj_description, can_pickup_obj, obj_starting_loc, is_hidden, unhide_event_id):
         self.story_id = story_id
 
         self.obj_name = obj_name
@@ -38,4 +39,35 @@ class StoryObject:
     def add_to_server(self):
         conn = pymysql.connect(self.rds_host, user = self.name, passwd = self.rds_password, db = self.db_name, connect_timeout = 5, cursorclass = pymysql.cursors.DictCursor)
         with conn.cursor() as cur:
-            pass
+            cur.execute(("SELECT count(*) FROM `objects` WHERE story_id = %s"), (self.story_id))
+            results = cur.fetchone()
+            self.obj_id = results["count(*)"] + 1
+            cur.execute(("INSERT INTO objects(story_id, obj_id, obj_starting_loc, obj_name, obj_description, can_pickup_obj, is_hidden, unhide_event_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s")
+                        , (self.story_id, self.obj_id, self.obj_starting_loc, self.obj_name, self.obj_description, self.can_pickup_obj, self.is_hidden, self.unhide_event_id))
+            conn.commit()
+        conn.close()
+
+    def get_id(self):
+        id = (self.story_id, self.obj_id)
+        return id
+
+    def get(self, story_id, obj_id):
+        conn = pymysql.connect(self.rds_host, user = self.name, passwd = self.rds_password, db = self.db_name, connect_timeout = 5, cursorclass = pymysql.cursors.DictCursor)
+        with conn.cursor() as cur:
+            cur.execute(("SELECT * FROM `objects` WHERE story_id = %s AND obj_id = %s"), (story_id, obj_id))
+            results = cur.fetchone()
+            if results is None:
+                return None
+            else:
+                return StoryObject(story_id, results["obj_name"], results["obj_description"], results["can_pickup_obj"], results["obj_starting_loc"], results["is_hidden"], results["unhide_event_id"])
+
+    def show_info(self):
+        conn = pymysql.connect(self.rds_host, user = self.name, passwd = self.rds_password, db = self.db_name, connect_timeout = 5, cursorclass = pymysql.cursors.DictCursor)
+        with conn.cursor() as cur:
+            cur.execute(("SELECT * FROM `objects` WHERE story_id = %s AND obj_id = %s"), (self.story_id, self.obj_id))
+            results = cur.fetchone()
+            if results is None:
+                return None
+            else:
+                return json.dumps(results)
+
