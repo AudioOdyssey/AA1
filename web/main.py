059c3cc04e94f97f9b2e41705e820576200fd772
sonnyghosh <ghoshsonny10@gmail.com>
2019-06-13 13:35:35 -0400

    updated web services for story decisions
    pls kill me

--- web/main.py
+++ web/main.py
@@ -239,17 +239,125 @@ def event_new():
     story_id = details['story_id']
     evnt = StoryEvent(story_id)
     evnt.add_to_server()
-    return redirect(url_for('event_show'))
+    events = StoryEvent.event_list(story_id)
+    return render_template("story/event/show.html", events = events, story_id = story_id)
 
 @app.route("/story/location/show")
 def location_show():
     locations= StoryLocation.loc_list(story_id)
     return render_template("story/location/show.html", locations=locations, story_id=story_id)
 
+@app.route('/story/location/update', methods = ['POST'])
+def location_update():
+    details = request.form
+    in_story_id = details['story_id']
+    if story_id is None:
+        in_story_id = story_id
+    loc_id = details['loc_id']
+    if loc_id is None:
+        loc_id = StoryLocation.get_last_id(story_id)
+    name = details['location-name']
+    original_desc = details['loc_origin_description']
+    short_desc = details['loc_short_description']
+    post_event_description = details['location_post_event_description']
+    event_id = details.get("location_event_id")
+    if event_id is None:
+        event_id = 0
+    auto_goto = details.get('auto_goto')
+    next_location_id = details.get('next_loc_id')
+    if next_location_id is None:
+        next_location_id = 0
+    loc = StoryLocation.get(in_story_id, loc_id)
+    loc.update(in_story_id, loc_id, name, original_desc, short_desc, post_event_description, event_id, auto_goto, next_location_id)
+    return redirect(url_for("location_show"))
+
+@app.route('/story/location/new', methods = ['POST'])
+def location_new():
+    details = request.form
+    in_story_id = details['story_id']
+    loc = StoryLocation(in_story_id)
+    loc.add_to_server()
+    return redirect(url_for("location_show"))
+
 @app.route("/story/location/decision/show")
 def decision_show():
-    decisions = [StoryDecision()]
+    decisions = StoryDecision(story_id)
     return render_template("story/location/decision/show.html", decisions=decisions, story_id=story_id, location_id=1)
+
+@app.route("/story/location/decision/update", methods = ['POST'])
+def decision_update():
+    details = request.form
+    decision_id = details['decision_id']
+    if decision_id is None:
+        decision_id = StoryDecision.get_last_id(story_id)
+    location_id = details['location_id']
+    decision_name = details['decision-name']
+    sequence = details['sequence_number']
+    transition = details.get('transition')
+    if transition is None:
+        transition = False
+    else:
+        transition = True
+    transition_loc_id = details.get("transition_loc_id") 
+    if transition_loc_id is None:
+        transition_loc_id = 0
+    is_hidden = details.get('hidden')
+    if is_hidden is None:
+        is_hidden = False
+    else:
+        is_hidden = True
+    show_event_id = details.get("show_event_id")
+    if show_event_id is None:
+        show_event_id = 0
+    show_object_id = details.get("show_object_id")
+    if show_object_id is None:
+        show_object_id = 0
+    is_locked = details.get("locked")
+    if is_locked is None:
+        is_locked = False
+    else:
+        is_locked = True
+    locked_descr = details.get('locked_descr')
+    if locked_descr is None:
+        locked_descr = ''
+    unlock_event_id = details.get('unlock_event_id')
+    if unlock_event_id is None:
+        unlock_event_id = 0
+    unlock_obj_id = details.get("unlock_obj_id")
+    if unlock_obj_id is None:
+        unlock_obj_id = 0
+    aftermath_desc = details['aftermath_desc']
+    cause_event = details.get('cause_event')
+    if cause_event is None:
+        cause_event = False
+    else:
+        cause_event = True
+    effect_event_id = details.get('effect_event_id')
+    if effect_event_id is None:
+        effect_event_id = 0
+    dec_description = details['dec_description']
+    can_occur_once = details.get("can_occur_once")
+    if can_occur_once is None:
+        can_occur_once = False
+    else:
+        can_occur_once = True
+    is_locked_by_event_id = details.get("is_locked_by_event_id")
+    if is_locked_by_event_id is None:
+        is_locked_by_event_id = 0
+    locked_by_event_desc = details.get("locked_by_event_description")
+    if locked_by_event_desc is None:
+        locked_by_event_desc = ""
+    dec = StoryDecision.get(story_id, location_id, decision_id)
+    dec.update(story_id, decision_id, location_id, sequence, decision_name, transition, transition_loc_id, is_hidden, is_locked, dec_description, show_event_id, show_object_id, unlock_event_id, unlock_obj_id, locked_descr, aftermath_desc, cause_event, effect_event_id, can_occur_once, is_locked_by_event_id, locked_by_event_desc)
+    return redirect(url_for("decision_show"))
+
+@app.route("/story/location/decision/new", methods = ['POST'])
+def decision_new():
+    details = request.form
+    location_id = details['location_id']
+    dec = StoryDecision(story_id, location_id)
+    dec.add_to_server()
+    return redirect(url_for("decision_show"))
 
 @app.route("/about")
 def about():
@@ -259,15 +367,15 @@ def about():
 def contact():
     return render_template("contact.html")
 
-@login_manager.user_loader
-def load_user(user_id):
-    usr = User()
-    return usr.get(user_id)
-
 @app.route("/verification/view")
 def verification_view():
     stories = [Story(5, "Story Title", "Brian", "Short Synopsis", 50, True, "Fiction", 3, 30, 50, False, None, None, "not verified", 0.0, 1, 16.3, False, False)]
     return render_template("verification/view.html" , stories=stories)
+
+@login_manager.user_loader
+def load_user(user_id):
+    usr = User()
+    return usr.get(user_id)
 
 if __name__=='__main__':
 	app.run()
