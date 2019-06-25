5468cf59e891b977c5dd8fea5dec6c26d86d90fc
Brian Duffy <bgd1112@gmail.com>
2019-06-25 10:43:37 -0400

    Now submitting changes in the form

---
+++ web/static/js/story/update.js
@@ -0,0 +1,12 @@
+function story_changed(elem) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        } else if (this.readyState == 4) {
+            console.log(this.responseText);
+        }
+    };
+    xhttp.open("POST", "/story/update", true);
+    xhttp.send(new FormData(elem.form));
+}
