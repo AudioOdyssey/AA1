6134b65b5b9ce7f00a07ce46b51ae87a3f963756
Brian Duffy <bgd1112@gmail.com>
2019-06-26 13:26:35 -0400

    Rigged up story deletion

--- web/static/js/story/update.js
+++ web/static/js/story/update.js
@@ -10,3 +10,18 @@ function story_changed(elem) {
     xhttp.open("POST", "/story/update", true);
     xhttp.send(new FormData(elem.form));
 }
+
+function story_delete(story_id) {
+    if (!confirm("You are sure you want to delete this story?"))
+        return;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        } else if (this.readyState == 4) {
+            console.log(this.responseText);
+        }
+    };
+    xhttp.open("POST", "/story/destroy", true);
+    xhttp.send("story_id=" + story_id);
+}
