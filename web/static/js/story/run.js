48b014421fbbab81556473c1565c28c2af19f96d
Brian Duffy <bgd1112@gmail.com>
2019-07-10 16:34:40 -0400

    More fields for execution cycle

--- web/static/js/story/run.js
+++ web/static/js/story/run.js
@@ -23,7 +23,7 @@ function getCookie(cname) {
 function addItem(item) {
     var items = getCookie("rundata");
     if (items == "")
-        items = '{"items":[], "events":[]}'
+        items = '{"items":[], "events":[], "decs":[], "back":[]}'
     var obj = JSON.parse(items);
     if (obj.items.indexOf(item) === -1) obj.items.push(item);
     var cookie = JSON.stringify(obj);
@@ -33,9 +33,47 @@ function addItem(item) {
 function addEvent(event) {
     var events = getCookie("rundata");
     if (events == "")
-        events = '{"items":[], "events":[]}'
+        events = '{"items":[], "events":[], "decs":[], "back":[]}'
     var obj = JSON.parse(events);
     if (obj.events.indexOf(item) === -1) obj.events.push(item);
     var cookie = JSON.stringify(obj);
     setCookie("rundata", cookie, 1);
+}
+
+function addDec(dec) {
+    var events = getCookie("rundata");
+    if (events == "")
+        events = '{"items":[], "events":[], "decs":[], "back":[]}'
+    var obj = JSON.parse(events);
+    if (obj.decs.indexOf(dec) === -1) obj.decs.push(dec);
+    var cookie = JSON.stringify(obj);
+    setCookie("rundata", cookie, 1);
+}
+
+function addBack(back) {
+    var events = getCookie("rundata");
+    if (events == "")
+        events = '{"items":[], "events":[], "decs":[], "back":[]}'
+    var obj = JSON.parse(events);
+    obj.back.push(back);
+    var cookie = JSON.stringify(obj);
+    setCookie("rundata", cookie, 1);
+}
+
+function decDone(dec) {
+    var events = getCookie("rundata");
+    if (events == "")
+        events = '{"items":[], "events":[], "decs":[], "back":[]}'
+    var obj = JSON.parse(events);
+    return obj.events.indexOf(item) === -1
+}
+
+function dec_clicked(story_id, dec_id, transition, transition_loc_id, can_occur_once, cause_event, effect_event_id) {
+    if (cause_event)
+        addEvent(effect_event_id);
+    if (can_occur_once && !decDone())
+        addDec(dec_id);
+    if (transition)
+        addBack(transition_loc_id);
+        window.location.href = "/story/run?story_id=" + story_id + "&location_id=" + transition_loc_id
 }
