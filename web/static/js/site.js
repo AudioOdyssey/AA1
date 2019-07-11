44d148feaac67d6525bf8e5d97387114af5d2a9f
Brian Duffy <bgd1112@gmail.com>
2019-07-11 14:51:00 -0400

    Fixed Save buttons
    Save buttons now display properly after the first run

--- web/static/js/site.js
+++ web/static/js/site.js
@@ -1,5 +1,7 @@
 function save() {
     // story_changed(document.getElementById("edit-story-details"));
+    document.getElementById("save-bubble").style.opacity = 1;
+    document.getElementById("save-bubble").style.filter = 'alpha(opacity=100)';
     document.getElementById("save-bubble").style.display = "block";
     window.setTimeout(savedone,1500);
 }
