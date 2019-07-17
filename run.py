23f13f475d4f6320f3ba4d13fb083e61f8c1c325
Brian Duffy <bgd1112@gmail.com>
2019-07-17 15:03:52 -0400

    Changed secret key to be more secure for debugging

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ import logging
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0,"./audio_od")
 from main import app as application
-application.secret_key = b"jk_\xf7\xa7':\xea$/\x88\xc0\xa3\x0e:d"
+application.secret_key = b"()Hdy)(D^tUBTyi*T*tgI*B"
 
 application.run()
