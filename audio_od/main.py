3c4872d1b610b7284df9ae57661405f1a8ed04a8
sonnyghosh <ghoshsonny10@gmail.com>
2019-07-25 15:51:12 -0400

    Moved all the story-related methods to its own file

--- audio_od/main.py
+++ audio_od/main.py
@@ -40,35 +40,9 @@ UPLOAD_FOLDER = config.upload_folder
 
 # oauth = OAuth(app)
 
-# refresh_t = None
 
-
-# google_blueprint = make_google_blueprint(client_id=config.google_client_id, client_secret=config.google_client_secret, scope=["profile","email"])
-
-# app.register_blueprint(google_blueprint, url_prefix='/google_login'
 
 random.seed()
-
-# google_client=WebApplicationClient(GOOGLE_CLIENT_ID)
-
-
-
-
-
-
-
-
-# @app.route('/facebook/login')
-# def facebook_login():
-#     redirect_uri= url_for('facebook_authorize', _external=True)
-#     return oauth.facebook.authorize_redirect(redirect_uri)
-
-
-# @app.route('/facebook/authenticate')
-# def facebook_authenticate():
-#     token = oauth.facebook.authorize_access_token()
-#     resp = outh.facebook.
-
 
 @app.before_first_request
 def load_id():
@@ -86,91 +60,13 @@ def load_id():
     return resp
 
 
-@app.route("/story/update", methods=["GET"])
-@authentication_required
-@check_header
-def story_update():
-    story = Story.get(int(request.args['story_id']))
-    if story.user_creator_id != getUid() and not checkEditorAdmin(getUid()):
-        abort(403)
-    objects = StoryObject.obj_list(request.args['story_id'])
-    events = StoryEvent.event_list(request.args['story_id'])
-    locations = StoryLocation.loc_list(request.args['story_id'])
-    coverimage = story.get_image_base64().decode("utf-8")
-    return render_template("story/update.html", StoryLocation=StoryLocation, story=story, objects=objects, events=events, locations=locations, coverimage=coverimage)
 
 
-@app.route("/story/image")
-@authentication_required
-@check_header
-def story_image():
-    story = Story.get(int(request.args['story_id']))
-    if story.user_creator_id != getUid() and not checkEditorAdmin(getUid()):
-        abort(403)
-    # print(story.get_image_base64())
-    return story.get_image_base64()
 
 
-valid_genres = {"Mystery", "Romance", "Sci-Fi", "Fantasy", "Historical Fiction", "Drama",
-                "Horror", "Thriller", "Comedy", "Adventure", "Sports", "Non-Fiction", "Other Fiction"}
 
 
-@app.route("/story/update", methods=["POST"])
-@authentication_required
-def story_update_post():
-    details = request.form
-    story_id = request.form.get('story_id')
-    story = Story.get(story_id)
-    if story.user_creator_id != getUid() and not checkEditorAdmin(getUid()):
-        abort(403)
-    story_title = details['story_title']
-    story_synopsis = details['story_synopsis']
-    story_price = details['story_price']
-    genre = details.get('genre')
-    if genre is None or genre not in valid_genres:
-        genre = "Miscellaneous"
-    story.length_of_story = details['length_of_story']
-    story.inventory_size = details.get('inventory_size')
-    story.starting_loc = details.get('starting_loc')
-    story.story_id = story_id
-    filename = ''
-    file = request.files['cover']
-    if file.filename == '':
-        pass
-    if file and allowed_file(file):
-        filename = str(story_id) + ".jpg"
-        file.save(os.path.join(UPLOAD_FOLDER, 'covers', filename))
-    story.verification_status = 0
-    story.update_verify()
-    story.update(story_title, "", story_price, 0, genre, story_synopsis)
-    #story_title, story_author, story_price, story_language_id, length_of_story, genre, story_synopsis, inventory_size
-    return '{"status":"ok"}'
 
-
-@app.route("/story/destroy", methods=["POST"])
-@authentication_required
-def story_destroy():
-    story = Story.get(request.args['story_id'])
-    if story.user_creator_id != getUid() and not checkAdmin(getUid()):
-        abort(403)
-    Story.destroy(request.args['story_id'])
-    return '{"status":"ok"}'
-
-valid_mimetypes = ['image/jpeg', 'image/png', 'image/bmp']
-
-def allowed_file(file):
-    mimetype = file.content_type
-    return mimetype in valid_mimetypes
-
-
-@app.route("/story/new", methods=["POST"])
-@authentication_required
-def story_new():
-    uid = getUid()
-    story = Story(user_creator_id=uid)
-    story.story_synopsis = ""
-    story.add_to_server()
-    return '{"status":"ok", "story": {"story_id":' + str(story.story_id) + '}}'
 
 
 @app.route("/story/object/show")
@@ -189,21 +85,6 @@ def object_show():
     return render_template("story/object/show.html", locations=locations, events=events, objects=objects, story_id=story_id)
 
 
-@app.route("/app/story/info", methods=['GET'])
-def app_story_logistics():
-    return Story.get_entities(int(request.args.get('story_id')))
-
-
-@app.route("/app/store", methods=["GET"])
-def app_store_info():
-    return Story.display_for_store()
-
-
-@app.route("/store/story/info", methods=['GET'])
-def app_store_expand():
-    details = request.json
-    story_id = details.get("story_id")
-    return Story.get_info(story_id)
 
 
 
@@ -862,11 +743,6 @@ def story_run():
     return render_template("story/run.html", inv=inv, evts=evts, triggered=triggered, backs=backs, objects=objects, decisions=decisions, StoryEvent=StoryEvent, StoryLocation=StoryLocation, StoryObject=StoryObject, story=story, location=location)
 
 
-@app.route("/story/help")
-@authentication_required
-@check_header
-def help():
-    return render_template("story/help.html")
 
 
 @app.route("/verification/help")
