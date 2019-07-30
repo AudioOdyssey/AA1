0750d0a462d23dc8b08d85ba6fb5d3d742a4f166
sonnyghosh <ghoshsonny10@gmail.com>
2019-07-29 22:08:00 -0400

    fixed errors with merging

--- audio_od/__init__.py
+++ audio_od/__init__.py
@@ -4,14 +4,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-<<<<<<< Updated upstream
 
 from audio_od import config, auth, home, userprofile, verification, storyinfo, locationinfo, storyobjectinfo, eventinfo, decisioninfo
 
 app.config['SECRET_KEY'] = config.secret_key
 app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
 app.config['UPLOAD_FOLDER']=config.upload_folder
-=======
+
 import config
 app.config['SECRET_KEY'] = config.secret_key
 app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
@@ -21,4 +20,4 @@ from audio_od import home
 
 from audio_od.Auth import bp as auth_bp
 app.register_blueprint(auth_bp)
->>>>>>> Stashed changes
+
