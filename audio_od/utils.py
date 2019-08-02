ea7bebcaef50a240462c0ee8315ef231091a2ba4
Zoe Drasner <zdras1@icloud.com>
2019-08-02 16:12:01 -0400

    Update utils.py

--- audio_od/utils.py
+++ audio_od/utils.py
@@ -129,7 +129,7 @@ def server_error_500(e):
     return render_template('error/500.html'), 500
 
 def isValidEmail(email):
-""" Uses regex to check if the email is valid or not. Will return true if valid, else will return false."""
+    """Uses regex to check if the email is valid or not. Will return true if valid, else will return false."""
     if len(email) > 7:
         if re.match(r"^.+@(\[?)[a-zA-Z0-9-.]+.(([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$)", email) != None:
             return True
