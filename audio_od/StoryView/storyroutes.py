3f81f0e7badea09f502e6139018e67dd25001285
Brian Duffy <bgd1112@gmail.com>
2019-08-02 10:24:26 -0400

    Update storyroutes.py

--- audio_od/StoryView/storyroutes.py
+++ audio_od/StoryView/storyroutes.py
@@ -8,7 +8,7 @@ import base64
 from flask import redirect, render_template, request, Blueprint
 
 #Internal imports
-from models import Story, StoryObject, StoryLocation, StoryEvent
+from models import Story, StoryObject, StoryLocation, StoryEvent, StoryDecision
 from audio_od.utils import authentication_required, check_header, checkEditorAdmin, getUid
 
 story_view = Blueprint("story", __name__)
