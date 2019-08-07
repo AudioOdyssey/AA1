f991fd2573b8644ad025b828a5b081106391328e
Brian Duffy <bgd1112@gmail.com>
2019-08-07 11:49:37 -0400

    Fix for problem in linting

--- audio_od/__init__.py
+++ audio_od/__init__.py
@@ -4,22 +4,6 @@ from flask import Flask
 
 import config
 
-from audio_od.Auth.routes import auth as auth_bp
-from audio_od.Home.routes import home as home_bp
-from audio_od.Verification.routes import verification as verif_bp
-from audio_od.Users.routes import userprofile as user_bp
-
-from audio_od.StoryView.decisionroutes import dec_view as dec_bp
-from audio_od.StoryView.eventroutes import ev_view as ev_bp
-from audio_od.StoryView.locationroutes import loc_view as loc_bp
-from audio_od.StoryView.storyobjectroutes import obj_view as obj_bp
-from audio_od.StoryView.storyroutes import STORY_VIEW as story_bp
-from audio_od.Dashboard.routes import dash_view as dash_db
-
-from audio_od.Help.routes import helper as help_bp
-
-from audio_od import utils
-
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = config.secret_key
@@ -44,6 +28,22 @@ app.config['FACEBOOK_CLIENT_ID'] = config.facebook_client_id
 app.config['FACEBOOK_CLIENT_SECRET'] = config.facebook_client_secret
 
 
+from audio_od.Auth.routes import auth as auth_bp
+from audio_od.Home.routes import home as home_bp
+from audio_od.Verification.routes import verification as verif_bp
+from audio_od.Users.routes import userprofile as user_bp
+
+from audio_od.StoryView.decisionroutes import dec_view as dec_bp
+from audio_od.StoryView.eventroutes import ev_view as ev_bp
+from audio_od.StoryView.locationroutes import loc_view as loc_bp
+from audio_od.StoryView.storyobjectroutes import obj_view as obj_bp
+from audio_od.StoryView.storyroutes import STORY_VIEW as story_bp
+from audio_od.Dashboard.routes import dash_view as dash_db
+
+from audio_od.Help.routes import helper as help_bp
+
+from audio_od import utils
+
 
 app.register_blueprint(home_bp)
 app.register_blueprint(auth_bp)
@@ -56,5 +56,3 @@ app.register_blueprint(obj_bp)
 app.register_blueprint(story_bp)
 app.register_blueprint(dash_db)
 app.register_blueprint(help_bp)
-
-
